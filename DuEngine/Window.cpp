439dd8c64bf31ebd075b2ad66f5529db1b465ffb
ruofeidu <xiaoxinghai@gmail.com>
2018-02-20 17:16:15 -0500

    Candlestick.cmd

--- DuEngine/Window.cpp
+++ DuEngine/Window.cpp
@@ -14,7 +14,7 @@ Window* Window::GetInstance() {
 }
 
 void Window::init(int argc, char* argv[], int _width, int _height, string _windowTitle) {
-	clock_t beginTime = clock();
+	clock_t initTime = clock();
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutCreateWindow(_windowTitle.c_str());
@@ -24,5 +24,5 @@ void Window::init(int argc, char* argv[], int _width, int _height, string _windo
 	if (GLEW_OK != err) {
 		std::cerr << "Error: " << glewGetString(err) << std::endl;
 	}
-	info("Window Creation costs: " + to_string(float(clock() - beginTime) / CLOCKS_PER_SEC) + " s");
+	info("Window Creation costs: " + to_string(float(clock() - initTime) / CLOCKS_PER_SEC) + " s");
 }
---
+++ SetEnvironmentVariable.cmd
@@ -0,0 +1,4 @@
+setx OPENCV_INC "OpenCV"
+setx OPENCV_LIB "OpenCV"
+setx GLEW_INC "OpenCV"
+setx GLEW_LIB "OpenCV"
